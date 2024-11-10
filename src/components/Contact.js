4809d439f437a1b4de508ff704cec45980422efc
BilalAhmedMalik1 <bilalahmedmalik2005@gmail.com>
2024-11-10 22:20:31 +0500

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="page-container">
+      <h1>Contact Us</h1>
+      <p>You can reach us at <a href="mailto:contact@example.com">contact@example.com</a></p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="page-container">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the homepage of our simple multi-page website.</p>
+    </div>
+  );
+}
+
+export default Home;
